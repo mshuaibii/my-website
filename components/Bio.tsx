bee6d113a31d36f24495daf728f339b1f93897c4
Muhammed Shuaibi <45150244+mshuaibii@users.noreply.github.com>
2025-10-27 20:59:29 -0500

    Change contact email in Bio component
    Updated contact email in the Bio component.

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -10,9 +10,10 @@ export default function Bio() {
 	I am a Research Engineer on the FAIR Chemistry team. My current research focuses on deep learning applications to addressing broad challenges in molecules and materials discovery. Building open datasets, frameworks, and tools to accelerate community research is something I am particularly fond of. Before that, I completed my PhD in Chemical Engineering with Zachary W. Ulissi.
         </p>
 	<p className="text-start text-muted-foreground lg:px-6">
-	Feel free to get in touch: mshuaibi@meta.com
+	Feel free to get in touch: mushuaibi@gmail.com
         </p>
       </div>
     </section>
   );
 }
+
